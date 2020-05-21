5ef5de4867c31c88ad0992f51c49e9e1203688a8
betoxd40 <alberto@blockfi.com>
2020-05-21 17:15:50 -0300

    Fixed config

--- config/config.exs
+++ config/config.exs
@@ -2,8 +2,17 @@ use Mix.Config
 
 config :commanded_scheduler,
   ecto_repos: [Commanded.Scheduler.Repo],
+  event_stores: [Commanded.Scheduler.EventStore]
   schedule_interval: 60_000,
   max_retries: 3,
   job_timeout: :infinity
+
+config :conduit, Commanded.Scheduler.App,
+  event_store: [
+    adapter: Commanded.EventStore.Adapters.EventStore,
+    event_store: Commanded.Scheduler.EventStore
+  ],
+  pub_sub: :local,
+  registry: :local
 
 import_config "#{Mix.env()}.exs"
