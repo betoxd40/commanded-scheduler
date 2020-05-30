2c0fca85ee3afb2593b1275573a8ffda33dacbf9
betoxd40 <alberto@blockfi.com>
2020-05-30 11:29:48 -0300

    Debug

--- lib/commanded/scheduler/scheduling/dispatcher.ex
+++ lib/commanded/scheduler/scheduling/dispatcher.ex
@@ -3,11 +3,11 @@ defmodule Commanded.Scheduler.Dispatcher do
 
   require Logger
 
-  alias Commanded.Scheduler.Router
-
   @behaviour Commanded.Scheduler.Job
 
   def execute(schedule_uuid, command) do
+    IO.puts "Commanded.Scheduler.DispatcherCommanded.Scheduler.DispatcherCommanded.Scheduler.Dispatcher"
+    IO.puts "Attempting to trigger schedule #{inspect schedule_uuid} #{inspect command}"
     Logger.debug(fn -> "Attempting to trigger schedule #{inspect schedule_uuid}" end)
 
     Commanded.Scheduler.App.dispatch(command)
