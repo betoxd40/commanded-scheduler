ff97bb2b329175427aaf18a32283405483151d87
betoxd40 <alberto@blockfi.com>
2020-05-31 12:00:57 -0300

    wip

--- lib/commanded/scheduler/schedule/schedule.ex
+++ lib/commanded/scheduler/schedule/schedule.ex
@@ -26,7 +26,12 @@ defmodule Commanded.Scheduler.Schedule do
   # Schedule a one-off command
   def execute(%Schedule{schedule_uuid: nil} = schedule, %ScheduleOnce{} = once) do
     IO.puts("execue %Schedule{schedule_uuid: nil} = schedule, %ScheduleOnce{} = once")
-    %ScheduledOnce{}
+    %ScheduledOnce{
+      schedule_uuid: "21576cc8-bedd-4bd5-8824-1246884cd321",
+      name: "no te interesa",
+      command: %{},
+      due_at: nil
+    }
   end
 
   def execute(%Schedule{}, %ScheduleOnce{}), do: {:error, :already_scheduled}
